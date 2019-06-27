1f5274d185d019334da741b33c5f9a4cef7268e2
mersadhj <48446076+mersadhj@users.noreply.github.com>
2019-06-27 22:22:46 +0430

    Residenge Manager Added

--- RealState/ResidenceManager.h
+++ RealState/ResidenceManager.h
@@ -1,9 +1,33 @@
 #pragma once
+#include <functional>
+#include "Residence.h"
+#include <list>
+using std::list;
 class ResidenceManager
 {
+	list<ApartmentBuilding> aparments;
+	string filename;
+	list<Residence*> residences;
 public:
-	ResidenceManager();
-
+	void delete_residence(int id);
+	void edit_residence(Residence&);
+	void add_residence(Residence* residence);
+	list<Residence*>& get_residences() {//apartment
+		return residences;
+	}
+	Residence* query_residence(int id);//apartment
+	list<Residence*> query_residence(std::function<bool (Residence*&)>);
+	void delete_apartment(int id);
+	void edit_apartment(ApartmentBuilding&);
+	void add_apartment(ApartmentBuilding& apartment);
+	list<ApartmentBuilding>& get_apartment() {
+		return aparments;
+	}
+	Residence* query_residence(int id);
+	list<Residence*> query_residence(std::function<bool(Residence*&)>);
+	void save();
+	void load();
+	explicit ResidenceManager(const string &filename = "residences.txt");
 	~ResidenceManager();
 };
 
